bf7438d456860ed0bbd4179d78b7812379d83b41
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 22:01:32 +0530

    Add project files.

---
+++ UserService/Database/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+
+namespace UserService.Database.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+
+    }
+}
